50fdf399c709cf30cff824e3e139ca9291f624c6
Karol Żak <karol.zak@hotmail.com>
2017-04-27 06:47:09 +0200

    minor fixes

--- XamMvvmAndWebServices.Client/XamMvvmAndWebServices.UWP/Services/DialogService.cs
+++ XamMvvmAndWebServices.Client/XamMvvmAndWebServices.UWP/Services/DialogService.cs
@@ -11,7 +11,7 @@ namespace XamMvvmAndWebServices.UWP.Services
     //SHOW: IDialogService - Windows implementation
     public class DialogService : IDialogService
     {
-        public void Alert(string message, string title, string okbtnText)
+        public async void Alert(string message, string title, string okbtnText)
         {
 
             var dlg = new MessageDialog(message,title);
@@ -19,7 +19,7 @@ namespace XamMvvmAndWebServices.UWP.Services
             {
             }
             ));
-            dlg.ShowAsync();
+            await dlg.ShowAsync();
         }
     }
 }
